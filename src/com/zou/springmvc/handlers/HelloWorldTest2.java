ee83bdc31777f73607cbfb9c30555540484aef7e
zoulvzhou <940933073@qq.com>
2018-05-26 14:36:39 +0800

    SpringMVC 各种请求参数

--- src/com/zou/springmvc/handlers/HelloWorldTest2.java
+++ src/com/zou/springmvc/handlers/HelloWorldTest2.java
@@ -1,6 +1,9 @@
 package com.zou.springmvc.handlers;
 
 
+import com.zou.springmvc.bean.User;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -34,8 +37,88 @@ public class HelloWorldTest2 {
 
     }
 
-}
 
+    /**
+     * 设置请求头或者请求参数
+     */
+
+    @RequestMapping(value = "testParamsAndHeaders", params = {"username", "age != 10"})
+    public String testParamsAndHeaders() {
+
+        System.out.println("testParamsAndHeaders");
+
+        return SUCCESS;
+    }
+
+
+    /**
+     * 通配符
+     * <p>
+     * ** 表示任意多个字符
+     */
+
+    @RequestMapping("/testAntPath/**/testAntPath")
+    public String testAntPath() {
+
+        System.out.println("testAntPath");
+        return SUCCESS;
+    }
+
+
+    /**
+     * @PathVariable 占位符
+     * <p>
+     * 可以用来映射URL中的占位符到目标方法的参数中
+     */
+    @RequestMapping("/testPathVariable/{id}")
+    public String testPathVariable(@PathVariable(value = "id") Integer id) {
+
+        System.out.println("testPathVariable: " + id);
+
+        return SUCCESS;
+
+    }
+
+
+    @RequestMapping("/testCookieValue")
+    public String testCookieValue(@CookieValue("JSESSIONID") String sessionId) {
+
+
+        System.out.println("testCookieValue: sessionId : " + sessionId);
+
+        return SUCCESS;
+
+    }
+
+
+    /**
+     * 使用POJO作为参数
+     * 当你请求的是一个对象，并且存在级联关系的时候
+     */
+
+    @RequestMapping("/testPojo")
+    public String testPojo(User user) {
+
+        System.out.println("testPojo: " + user);
+
+        return SUCCESS;
+    }
+
+
+    /**
+     *
+     */
+//    @RequestMapping("/testServletAPI")
+//    public String testServletAPI(HttpServletRequest request,
+//                                 HttpServletResponse response) {
+//        System.out.println("testServletAPI: " + request + "," + response);
+//
+//        return SUCCESS;
+//
+//    }
+
+
+}
 
 
 
--- web/index.jsp
+++ web/index.jsp
@@ -7,30 +7,80 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
+<head>
     <title>helloworld</title>
-  </head>
-  <body>
+</head>
+<body>
 
 
-  <! 指定的是post请求，那么需要使用表单提交 -->
-  <form action="springmvc/testMethod" method="POST">
+<a href="springmvc/testServletAPI">testServletAPI</a>
+
+
+<<br><<br>
+
+
+<form action="springmvc/testPojo" method="POST">
+
+    username: <input type="text" name="username">
+    <br>
+    password: <input type="password" name="password">
+    <br>
+    email: <input type="text" name="email">
+    <br>
+    city: <input type="text" name="address.city">
+    <br>
+    province: <input type="text" name="address.province">
+    <br>
+    <input type="submit" value="Submit">
+
+</form>
+
+
+<<br><<br>
+
+
+<a href="springmvc/testCookieValue">testCookieValue</a>
+
+
+<<br><<br>
+
+
+<a href="springmvc/testPathVariable/1">testPathVariable</a>
+
+
+<<br><<br>
+
+
+<a href="springmvc/testAntPath/xxx/yyy/testAntPath">testAntPath</a>
+
+
+<<br><<br>
+
+
+<a href="springmvc/testParamsAndHeaders?username=zou&age=11">testParamsAndHeaders</a>
+
+
+<<br><<br>
+
+
+<! 指定的是post请求，那么需要使用表单提交 -->
+<form action="springmvc/testMethod" method="POST">
 
     <input type="submit" value="submit">
 
-  </form>
+</form>
 
 
-  <a href="springmvc/testMethod">testMethod</a>
+<a href="springmvc/testMethod">testMethod</a>
 
 
-  <<br><<br>
+<<br><<br>
 
-  <a href="springmvc/testRequestMapping">testRequestMaping</a>
+<a href="springmvc/testRequestMapping">testRequestMaping</a>
 
 
-  <<br><<br>
+<<br><<br>
 
-  <a href="helloworld">Hello  World</a>
-  </body>
+<a href="helloworld">Hello World</a>
+</body>
 </html>
